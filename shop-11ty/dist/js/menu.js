156111a4a99f67fc4236348cf90e40d8a3749b81
ANeria <pandapandapandapanda30@gmail.com>
2022-04-26 10:56:04 -0400

    working on menu
    not sure what's wrong with code </3

---
+++ shop-11ty/dist/js/menu.js
@@ -0,0 +1,151 @@
+const menuFood = [
+    {
+        url: "https://live.staticflickr.com/24/50592825_f72a0fc29e_c.jpg",
+        item: "MENSTRUAL MEAL",
+        description: "It's bloody delicious",
+        price: 12,
+      },    {
+        url: "https://i.pinimg.com/736x/7e/9b/81/7e9b81180613a52a4b41c3a4e709cc88.jpg",
+        item: "KITTEN KRUNCH",
+        description: "The hairball is part of the experience",
+        price: 7,
+      },    
+      {
+        url: "https://64.media.tumblr.com/0a677be522bb667726794e880d672e59/0fc19b9f1da6a59e-d0/s1280x1920/975942ab4e002123c69b1b8a6b47569f23529d8d.jpg",
+        item: "FORG SAMCH",
+        description: "Pretty self-explanatory, but it's a frog sandwich",
+        price: 10,
+      },
+      {
+        url: "http://2.bp.blogspot.com/-aHVgoooY6k4/U8P2U0PpNQI/AAAAAAAAAbI/mqsWChyC4eg/w1200-h630-p-k-no-nu/86+nasty.jpg",
+        item: "NASTY PATTY",
+        description: "Find out where it's oozing from",
+        price: 12,
+      },    
+  ];
+
+const menuDessert = [
+  {
+    url: "https://spoonuniversity.com/wp-content/uploads/sites/20/2016/03/ice-cream-sandwich-secretmenus.com_.png",
+    item: "ICE CREAM SANDWICH",
+    description: "For your ice cream sandwich needs",
+    price: 5,
+  },
+{
+  url: "https://www.thefoodinmybeard.com/content/burgers/lucky/luckyburger7.jpg",
+  item: "LEPRECHAUN BURGER",
+  description: "I definitely don't feel lucky right now",
+  price: 8,
+},
+{
+  url: "https://cleobuttera.com/wp-content/uploads/2015/05/melting-text.jpg",
+  item: "BITE-ME BURGER",
+  description: "It's got bite to it",
+  price: 6,
+},
+{
+  url: "http://ocdn.eu/images/pulscms/ZjQ7MDA_/22011ec17d8af96d65cc66e5d242fd57.jpeg",
+  item: "POOPY PATTY",
+  description: "Someone made an oopsie",
+  price: 7,
+},
+
+]
+
+  const menuDrink = [ 
+    {
+    url: "https://ak.picdn.net/shutterstock/videos/33407110/thumb/1.jpg",
+    item: "SPARKLING SAUSAGE WATER",
+    description: "It sparkles cause it's boiling",
+    price: 4,
+  },    
+  {
+    url: "https://i.redd.it/jh1o2k5f8w451.jpg",
+    item: "CHUNKY MILK",
+    description: "Milk, with all of that wholesome pulp",
+    price: 7,
+  },
+  {
+    url: "https://i0.wp.com/investvine.com/wp-content/uploads/lao-lao-whiskey.jpg?fit=406%2C446&ssl=1",
+    item: "EVERYTHING SHOT",
+    description: "I feel it crawling in my throat",
+    price: 10,
+  },
+  {
+    url: "https://64.media.tumblr.com/811f38c34ffe057559eb061f1e0e3783/d6cafedf77815852-ca/s640x960/86235a1934afab53a2153ac935c17472d606c30f.jpg",
+    item: "SHOOK SHAKE",
+    description: "This shake will have you shook",
+    price: 12,
+  },
+]
+
+const menuSides = [
+  {
+    url: "https://pbs.twimg.com/media/FMXTZyGaQAAJTmc?format=jpg&name=small",
+    item: "PUTIN POUTINE",
+    description: "Invade your tastebuds instead",
+    price: 5,
+  },
+  {
+    url: "https://i.redd.it/xo5281e122f31.jpg",
+    item: "GOTTEM FRIES",
+    description: "Nice.",
+    price: 69,
+  },
+  {
+    url: "https://i.redd.it/honfoulvl2i61.jpg",
+    item: "CHEESY FRIES",
+    description: "Guess where the extra flavor comes from",
+    price: 8,
+  },
+  {url: "https://images.theconversation.com/files/248679/original/file-20181204-126668-10gu4at.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop",
+  item: "CRICKUSSY",
+  description: "Sushi with a show",
+  price: 7,
+},
+]
+  
+const merch = [ 
+{
+    url: "https://cdn.shopify.com/s/files/1/0726/7481/products/Exercise_Fries.jpg?v=1534701850",
+    item: "SOCK FRIES",
+    description: "Keep your food warm and your feet warmer",
+    price: 32,
+  },
+{
+    url: "images/chili slime.PNG",
+    item: "CHILI SLIME",
+    description: "Even the grease feels real",
+    price: 6,
+  },
+{
+    url: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/ap/odd%20unicorn%20meat-178534363_v2.jpg",
+    item: "UNICORN MEAT",
+    description: "You can technically eat it?",
+    price: 16,
+  },]
+
+  const homeHero = [
+    {
+      url: "https://ak.picdn.net/shutterstock/videos/33407110/thumb/1.jpg",
+      item: "SPARKLING SAUSAGE WATER",
+      price: 4,
+    },   
+      {
+        url: "https://pbs.twimg.com/media/FMXTZyGaQAAJTmc?format=jpg&name=small",
+        item: "PUTIN POUTINE",
+        price: 5,
+      },
+      {
+        url: "http://2.bp.blogspot.com/-aHVgoooY6k4/U8P2U0PpNQI/AAAAAAAAAbI/mqsWChyC4eg/w1200-h630-p-k-no-nu/86+nasty.jpg",
+        item: "NASTY PATTY",
+        price: 12,
+      },
+  ]
+  export { menuFood };
+  export { menuDrink };
+  export { menuDessert };
+  export { menuSides };
+  export { merch };
+  export {homeHero}
+  
